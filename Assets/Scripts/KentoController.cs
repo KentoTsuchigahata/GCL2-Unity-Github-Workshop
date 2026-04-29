4bbe67c890b9832a3e445e347ef7c98c813c8715
KentoTsuchigahata <kentotsuchigahata@gmail.com>
2026-04-29 09:45:58 +0800

    [Kento] Add sprite, script and GameObject

---
+++ Assets/Scripts/KentoController.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class KentoController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Mystery Box activated!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
